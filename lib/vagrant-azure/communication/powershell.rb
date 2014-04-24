e1324a46099f5a3a5fa70ca7ada565c5f59e1510
Ramakrishnan <ramakrishnan.v@happiestminds.com>
2014-04-24 07:09:02 +0530

    Fix check winrm running.
    Add guest port to create new ps session.

--- lib/vagrant-azure/communication/powershell.rb
+++ lib/vagrant-azure/communication/powershell.rb
@@ -2,7 +2,6 @@
 # Copyright (c) Microsoft Open Technologies, Inc.
 # All Rights Reserved. Licensed under the Apache 2.0 License.
 #--------------------------------------------------------------------------
-
 module VagrantPlugins
   module WinAzure
     module Communicator
